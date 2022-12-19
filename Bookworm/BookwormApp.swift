c9b1e7ecb31a4f988fd849822a672bf0425e00fa
Ruben Granet <ruben.granet@icloud.com>
2022-12-19 09:29:53 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ruben Granet on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
